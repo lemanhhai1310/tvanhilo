165ecd2843d7e02a35451fb0abfbdede91418614
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-05-27 23:26:03 +0700

    update

---
+++ hoadonphongthuy-gioithieu.php
@@ -0,0 +1,36 @@
+<?php $page = ""; ?>
+<?php include "header.php"; ?>
+<?php
+$data = array
+(
+    array(
+        'src' => 'imgs/bg1.jpg',
+        'title' => 'MẪU HÓA ĐƠN PHONG THỦY HILO GIỚI THIỆU',
+        'sub' => array(
+            'giới thiệu',
+            'tra cứu',
+            'kim',
+            'mộc',
+            'thổ',
+            'thủy',
+            'hỏa',
+        ),
+    ),
+);
+foreach ($data as $k1 => $v1) { ?>
+    <?php include "block1.php"; ?>
+<?php } ?>
+<div class="uk-section">
+    <div class="uk-container">
+        <h2 class="title2 title9 uk-text-center">HÓA ĐƠN PHONG THỦY HILO - ĐÓN MAY MẮN, RƯỚC TÀI LỘC</h2>
+        <div class="uk-column-1-2">
+            <p>Ngũ Hành Phong Thủy là bộ môn hoa học phương Đông đã có từ hàng nghìn năm nay với mục đích giúp con người sống hòa hợp với thiên nhiên và vạn vật.</p>
+            <p>Ứng dụng Ngũ Hành Phong Thủy trong cuộc sống giúp con người một phần nào đó điều hóa các vận khí của tự nhiên xung quanh mình, làm sao cho phù hợp với bản mệnh, đem lại nhiều may mắn và loại trừ các vận rủi.</p>
+            <p>Trong cuộc sống hiện đại của người phương Đông, Ngũ Hành Phong Thủy vẫn được ứng dụng rộng rãi trong nhiều lĩnh vực như kiến trúc, kinh doanh … với hi vọng đem lại nhiều may mắn.</p>
+            <p>Ở các quốc gia như Nhật Bản, Trung Quốc, Việt Nam … bên cạnh sự nỗ lực và sáng suốt trong các quyết định kinh doanh, rất nhiều các doanh nghiệp lớn, nhỏ đều nghiên cứu ứng dụng Ngũ Hành Phong Thủy với mục tiêu tăng thêm các vận khí tốt, giúp doanh nghiệp gặp nhiều may mắn và cơ hội hơn.</p>
+            <p>Sau nhiều khi phân tích kỹ lưỡng, nghiêm túc và nhận được sự tư vấn từ các chuyên gia nghiên cứu Ngũ Hành Phong Thủy lâu năm, Hilo Invoice trân trọng đem đến cho Quý khách hàng Bộ 35 mẫu hóa đơn Ngũ Hành Phong Thủy với hi vọng được đồng hành cùng quý khách hàng, góp một phần nhỏ đem lại thêm may mắn trong công việc kinh doanh của Quý khách hàng.</p>
+        </div>
+        <h2 class="title2 title9 uk-text-center">MẪU HÓA ĐƠN PHONG THỦY GIÚP GÌ CHO BẠN?</h2>
+    </div>
+</div>
+<?php include "footer.php"; ?>
