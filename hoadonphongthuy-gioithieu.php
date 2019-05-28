0f6c287885dbf5aa631e1ce626913b167b60b692
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-05-29 02:10:26 +0700

    hóa đơn điện tử - mẫu hóa đơn

--- hoadonphongthuy-gioithieu.php
+++ hoadonphongthuy-gioithieu.php
@@ -31,6 +31,54 @@ foreach ($data as $k1 => $v1) { ?>
             <p>Sau nhiều khi phân tích kỹ lưỡng, nghiêm túc và nhận được sự tư vấn từ các chuyên gia nghiên cứu Ngũ Hành Phong Thủy lâu năm, Hilo Invoice trân trọng đem đến cho Quý khách hàng Bộ 35 mẫu hóa đơn Ngũ Hành Phong Thủy với hi vọng được đồng hành cùng quý khách hàng, góp một phần nhỏ đem lại thêm may mắn trong công việc kinh doanh của Quý khách hàng.</p>
         </div>
         <h2 class="title2 title9 uk-text-center">MẪU HÓA ĐƠN PHONG THỦY GIÚP GÌ CHO BẠN?</h2>
+        <div class="uk-child-width-1-3@m" uk-grid>
+            <div>
+                <img src="imgs/nguhanh.jpg" alt="">
+            </div>
+            <div>
+                <ul class="uk-list list3">
+                    <?php
+                    $data = array
+                    (
+                        'Lựa chọn mẫu hóa đơn hợp với mệnh của chủ doanh nghiệp mang ý nghĩa giúp cho sự phát triển của doanh nghiệp ngày càng vững chắc, cường thịnh và công việc kinh doanh gặp nhiều may mắn.',
+                        'Ngược lại nếu mẫu hóa đơn được nhận sự hỗ trợ từ mệnh phù hợp của chủ doanh nghiệp cũng sẽ được phát triển. Doanh nghiệp xuất được nhiều hóa đơn hơn, cũng có nghĩa bạn đang bán được nhiều hàng hóa, dịch vụ hơn, thu được nhiều tiền bạc hơn.',
+                        'Việc xuất hóa đơn mang mệnh hợp với chủ doanh nghiệp sang cho khách hàng không mang ý nghĩa rằng bạn sẽ mất đi một phần may mắn; Mà thực tế là bạn đang gửi những mầm may mắn cho mình vào kho bạc của khách hàng, mang ý nghĩa giúp cho bạn thuận lợi hơn trong công việc của mình với khách hàng đó.',
+                        'Hãy tham khảo thêm bản phân tích theo từng bản mệnh mà chúng tôi cung cấp trên website này để lựa chọn mẫu hóa đơn phù hợp nhất cho bản mệnh chủ doanh nghiệp của bạn nhé!',
+                    );
+                    foreach ($data as $k1 => $v1) { ?>
+                        <li><i class="fa fa-star" aria-hidden="true"></i><?= $v1; ?></li>
+                    <?php } ?>
+                </ul>
+            </div>
+            <div>
+                <ul class="uk-list list3">
+                    <?php
+                    $data = array
+                    (
+                        'Tùy theo bản mệnh, bạn nên chọn những mẫu hóa đơn tạo nên vượng khí, cát lợi cho chủ doanh nghiệp. Ví dụ: chủ doanh nghiệp mệnh Hải Trung Kim, bạn có thể chọn mẫu hóa đơn mệnh Thổ cơ bản hoặc tốt hơn nữa là mẫu hóa đơn mệnh Sa Trung Thổ.',
+                        'Có nên chọn mẫu hóa đơn trùng với bản mệnh không? Câu trả lời là vừa có, vừa không. Có những bản mệnh hợp với bản mệnh trùng, tuy nhiên cũng có những bản mệnh xung khắc ví dụ như 2 bản mệnh Kiếm Trung Kim.',
+                        'Trong trường hợp bản mệnh của chủ doanh nghiệp không tìm được mẫu mệnh sinh phù hợp, bạn có thể chọn những mẫu hóa đơn mà bản mệnh của chủ doanh nghiệp tốt cho mẫu hóa đơn.',
+                        'Lưu ý không nên chọn những mẫu hóa đơn mang chiều hướng chỉ tốt một chiều có lợi cho chủ doanh nghiệp hoặc ngược lại. Ví dụ như Sơn Hạ Hỏa và Dương Liễu Mộc. Bạn nên chọn những mẫu mà mệnh của chủ doanh nghiệp và mẫu hóa đơn hòa hợp, bổ trợ cho nhau phát triển.',
+                    );
+                    foreach ($data as $k1 => $v1) { ?>
+                        <li><i class="fa fa-star" aria-hidden="true"></i><?= $v1; ?></li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-section-small block2">
+    <div class="uk-container">
+        <figure class="uk-text-center">
+            <img src="imgs/processpt.png" alt="">
+            <figcaption class="uk-margin-small-top">Quy trình đăng ký sử dụng Hóa đơn phong thủy Hilo</figcaption>
+        </figure>
+    </div>
+</div>
+<div class="uk-section">
+    <div class="uk-container">
+
     </div>
 </div>
 <?php include "footer.php"; ?>
