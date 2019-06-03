8deaa6fe118eca2fe9aa84c86f31416039fd1b72
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-06-03 18:39:20 +0700

    HÓA ĐƠN PHONG THỦY HILO - tra cứu

---
+++ hoadonphongthuy-tracuu.php
@@ -0,0 +1,30 @@
+<?php $page = ""; ?>
+<?php include "header.php"; ?>
+<?php
+$data = array
+(
+    array(
+        'src' => 'imgs/bg1.jpg',
+        'title' => 'MẪU HÓA ĐƠN PHONG THỦY HILO GIỚI THIỆU',
+        'sub' => array(
+            'giới thiệu',
+            'tra cứu',
+            'kim',
+            'mộc',
+            'thổ',
+            'thủy',
+            'hỏa',
+        ),
+    ),
+);
+foreach ($data as $k1 => $v1) { ?>
+    <?php include "block1.php"; ?>
+<?php } ?>
+<div class="uk-section">
+    <div class="uk-container">
+        <div class="uk-overflow-auto">
+            <table></table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php"; ?>
